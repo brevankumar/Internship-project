f8f14706d5528cf361f5e6e402f92f659f80ba44
brevankumar <brevankumar@gmail.com>
2023-07-19 19:47:02 +0530

    commit

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import os
 import sys
-import pickle
+
 import numpy as np 
 import pandas as pd
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -20,37 +22,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
-        for i in range(len(models)):
+
+        for i in range(len(list(models))):
             model = list(models.values())[i]
-            # Train model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            
+            #model.fit(X_train, y_train)  # Train model
 
-            # Predict Testing data
-            y_test_pred =model.predict(X_test)
+            y_train_pred = model.predict(X_train)
 
-            # Get R2 scores for train and test data
-            #train_model_score = r2_score(ytrain,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            y_test_pred = model.predict(X_test)
 
-            report[list(models.keys())[i]] =  test_model_score
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
     except Exception as e:
-        logging.info('Exception occured during model training')
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
 def load_object(file_path):
     try:
-        with open(file_path,'rb') as file_obj:
+        with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
-    except Exception as e:
-        logging.info('Exception Occured in load_object function utils')
-        raise CustomException(e,sys)
 
-    
+    except Exception as e:
+        raise CustomException(e, sys)
